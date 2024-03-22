4fb82ae6b2be780d635dac386246d3ec89a44ef8
Aldin Huric <alhuric89@gmail.com>
2024-03-22 13:24:21 +0100

    add filter option

--- src/App.js
+++ src/App.js
@@ -28,10 +28,21 @@ class App extends Component {
 				},
 			);
 	}
+
+	onSearchChange = (event) => {
+		const searchField = event.target.value.toLowerCase();
+
+		// Always update the state, even when input value is empty
+		this.setState(() => {
+			return { searchField };
+		});
+	};
 	render() {
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
 		console.log('render');
-		const filteredMonsters = this.state.monsters.filter((monster) => {
-			return monster.name.toLowerCase().includes(this.state.searchField);
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLowerCase().includes(searchField);
 		});
 		return (
 			<div className="App">
@@ -39,14 +50,7 @@ class App extends Component {
 					className="search-box"
 					type="search"
 					placeholder="search monsters"
-					onChange={(event) => {
-						const searchField = event.target.value.toLowerCase();
-
-						// Always update the state, even when input value is empty
-						this.setState(() => {
-							return { searchField };
-						});
-					}}
+					onChange={onSearchChange}
 				/>
 
 				{filteredMonsters.map((monster) => {
