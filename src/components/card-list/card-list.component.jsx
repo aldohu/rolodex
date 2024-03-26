7d26459d2034a573f9949b72c730bfd2207ce9b9
Aldin Huric <alhuric89@gmail.com>
2024-03-26 15:43:11 +0100

    created styles and made new component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,12 +1,23 @@
 import { Component } from 'react';
+import CardContainer from '../card-container/card-container.component'; // Import the CardContainer component
 
+import './card-list.styles.css';
 class CardList extends Component {
 	render() {
 		const { monsters } = this.props;
 		return (
-			<div>
+			<div className="card-list">
 				{monsters.map((monster) => {
-					return <h1 key={monster.id}>{monster.name}</h1>;
+					const { name, email, id } = monster;
+					return (
+						<CardContainer
+							className="card-container"
+							key={id}
+							id={id} // Pass id, name, and email as props to CardContainer
+							name={name}
+							email={email}
+						/>
+					);
 				})}
 			</div>
 		);
